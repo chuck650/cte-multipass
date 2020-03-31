85d3846c6aed35b732b78f871d7c474acb9ce0cb
Chuck Nelson <nelsonch650@gmail.com>
2020-03-31 18:16:25 -0400

    Updated with additonal cloud-init options

--- cte-multipass.py
+++ cte-multipass.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 
 from jinja2 import Environment, FileSystemLoader
-import sys, yaml, os, pwd, pprint, crypt
+import sys, yaml, os, pwd, pprint, crypt, base64
 
 # Check if displaying verbose information
 if "-v" in sys.argv:
@@ -18,8 +18,10 @@ user = pwd.getpwuid(os.getuid())
 username = user.pw_name
 gecos = user.pw_gecos.split(',')[0]
 home = user.pw_dir
-key_files = []
-ssh_keys = []
+ssh_pub_key_files = []
+ssh_pri_key_files = []
+ssh_pub_keys = []
+ssh_pri_keys = []
 
 
 # Config the environment to load templates from the ./templates folder
@@ -43,27 +45,46 @@ else:
 passwd = crypt.crypt(pword, crypt.mksalt(crypt.METHOD_SHA512))
 
 # Expand the aliased path to an absolute path
-key_files.append(os.path.expanduser(data['rsa_pub_key_path']))
-key_files.append(os.path.expanduser(data['ecdsa_pub_key_path']))
+if 'rsa_pub_key_path' in data:
+    ssh_pub_key_files.append(os.path.expanduser(data['rsa_pub_key_path']))
+if 'ecdsa_pub_key_path' in data:
+    ssh_pub_key_files.append(os.path.expanduser(data['ecdsa_pub_key_path']))
+if 'rsa_pri_key_path' in data:
+    ssh_pri_key_files.append(os.path.expanduser(data['rsa_pri_key_path']))
+if 'ecdsa_pri_key_path' in data:
+    ssh_pri_key_files.append(os.path.expanduser(data['ecdsa_pri_key_path']))
 
 # if the key exists, add it to the list of ssh keys
-for file in key_files:
+for file in ssh_pub_key_files:
     if os.path.isfile(file):
         key = open(file, 'r').read()
-        ssh_keys.append(key)
+        ssh_pub_keys.append(key)
 
+# if the key exists, add it to the list of ssh keys
+for file in ssh_pri_key_files:
+    if os.path.isfile(file):
+        with open(file, 'r') as keyfile:
+            key = keyfile.read()
+        key_bytes = key.encode('utf-8')
+        b64data = base64.b64encode(key_bytes).decode('ascii')
+        name = os.path.basename(file)
+        ssh_pri_keys.append(dict(name=name,content=b64data))
+
+# Write out some debug info is user specified -v option on invocation
 if verbose:
     print("Executable Path:" + abspath, file=sys.stderr)
     print(passwd, file=sys.stderr)
-    for key in ssh_keys:
+    for key in ssh_pub_keys:
         print("SSH Public Key: " + key, file=sys.stderr)
     print("Input Variables:", file=sys.stderr)
     pprint.pprint(data, stream=sys.stderr,indent=4)
 
+# Render the templae with vars
 user_data = template.render(data,
     username=username,
     password=passwd,
-    ssh_keys=ssh_keys,
+    ssh_pub_keys=ssh_pub_keys,
+    ssh_pri_keys=ssh_pri_keys,
     gecos=gecos)
 
 print("\n\033[0;36m" + user_data + "\033[0m")
@@ -76,3 +97,4 @@ print("  multipass launch -n cte -c 2 -d 40G -m 16G --cloud-init user-data.yaml 
 f = open("user-data.yaml","w")
 f.write(user_data + "\n")
 f.close()
+os.chmod('user-data.yaml', 0o600)
